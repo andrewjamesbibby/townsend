2319bdf976729eb34de9026b0ed51bd29a572a1c
Andrew Bibby <andrew.bibby@yudu.com>
2022-03-20 19:39:53 +0000

    Remove welcome route

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\LegacyProductsController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 // Legacy Routes
 Route::get('legacy/products', [LegacyProductsController::class, 'index']);
 Route::get('legacy/products/{section}', [LegacyProductsController::class, 'show']);
